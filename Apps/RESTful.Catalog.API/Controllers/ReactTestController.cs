8cc9ecbbacac627f5cee9e8644a283aacec16609
Marat <marat_17.5@mail.ru>
2019-01-16 19:01:18 +0400

    for react js tests

---
+++ Apps/RESTful.Catalog.API/Controllers/ReactTestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RESTful.Catalog.API.Utilities.Infra;
+
+namespace RESTful.Catalog.API.Controllers
+{
+    public class ReactTestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("{email}/{password}")]
+        public IActionResult CallBackReact(string email, string password)
+        {
+            return Ok(ResponseSuccess.Create(email + "-" + password));
+        }
+    }
+}
